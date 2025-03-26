d9450857dd1b77c7cbda7709b6c0f3add57f259a
Lana Steen <l.steen@rs.ge>
2025-03-26 10:21:04 +0400

    manager namesfixed

--- aspnetcore-webapi-clean-architecture-main/MyApp.Api/MyApp.Core/Entities/ManagerEntity.cs
+++ aspnetcore-webapi-clean-architecture-main/MyApp.Api/MyApp.Core/Entities/ManagerEntity.cs
@@ -9,6 +9,9 @@ namespace MyApp.Core.Entities
     public class ManagerEntity
     {
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PersonalNumber { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public int? HotelId { get; set; } 
